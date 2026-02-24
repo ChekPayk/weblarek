8f2c43c898d8337004786d3c212ee51e8792c56f
chekpayk81 <chekpayk81@gmail.com>
2026-02-24 19:50:57 +0300

    Доописывала интерфейсы

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,31 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export type TPayment = 'online' | 'cash';
+export interface Product {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+export interface Buyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+}
+export interface Order extends Buyer {
+    total: number;
+    items: string[];
+}
+export interface OrderResult {
+    id: string;
+    total: number;
+}
+export interface ApiResponseList<T> {
+    total: number;
+    items: T[];
+}
